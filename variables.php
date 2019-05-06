4e2377d712b049535cef9131d6fb63d4d916af2a
omarferndz <omarferndz@gmail.com>
2019-05-06 13:21:01 -0400

    variables

---
+++ variables.php
@@ -0,0 +1,14 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title>Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+	$a = 1;
+	$b = 3.34;
+	$c = "Hola Mundo"; 
+ 	echo $a, "<br>", $b, "<br>", $c;
+?>
+</body>
+</html>
