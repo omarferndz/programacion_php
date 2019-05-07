0fa252373a3724a9848005a0f7e034418dba31e2
omarferndz <omarferndz@gmail.com>
2019-05-07 08:23:56 -0400

    bucles

---
+++ bucles.php
@@ -0,0 +1,18 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+Inicio<BR>
+<?php
+	$i = 0;
+	while ($i < 10)
+	{
+		echo "El valor de i es ", $i,"<br>";
+		$i++;
+	}		
+?>
+Final<BR>
+</body>
+</html>
