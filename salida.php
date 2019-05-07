a2ac528f71abddba712e6b79284ea598b6610bb7
omarferndz <omarferndz@gmail.com>
2019-05-07 08:42:38 -0400

    salida

---
+++ salida.php
@@ -0,0 +1,11 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+	printf("El numero dos con diferentes formatos: %d %f %.2f",2,2,2);		
+?>
+</body>
+</html>
