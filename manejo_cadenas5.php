7cdefe0e15568199a69d272e9e6ccdba73133327
omarferndz <omarferndz@gmail.com>
2019-05-08 11:38:55 -0400

    manejo de cadenas 5

---
+++ manejo_cadenas5.php
@@ -0,0 +1,16 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+strpos(cadena1, cadena2) . Busca la cadena2 dentro de cadena1 indicándonos la posición en la que se encuentra.
+*/
+
+echo strpos("Busca la palabra dentro de la frase", "frase"),"<br><br>";
+
+?>
+</body>
+</html>
