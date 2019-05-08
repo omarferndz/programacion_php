18599a46c819b0bd564282e294d37640fb22a2ad
omarferndz <omarferndz@gmail.com>
2019-05-08 11:38:01 -0400

    manejo de cadenas 1

---
+++ manejo_cadenas1.php
@@ -0,0 +1,15 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+strlen(cadena). Nos devuelve el numero de caracteres de una cadena
+*/
+echo strlen("12345"),"<br>";
+
+?>
+</body>
+</html>
