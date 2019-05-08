42504889d9ae10f58d45a90d8befe25a6916dea5
omarferndz <omarferndz@gmail.com>
2019-05-08 11:38:29 -0400

    manejo de cadenas 3

---
+++ manejo_cadenas3.php
@@ -0,0 +1,15 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+substr(cadena, inicio, longitud). Devuelve una subcadena de otra, empezando por inicio y de longitud longitud .
+*/
+echo substr("Devuelve una subcadena de otra",13,9),"<br><br>";
+
+?>
+</body>
+</html>
