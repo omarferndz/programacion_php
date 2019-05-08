dd1e0ec2d0eebe35a19dc329959a2ea748b8e7f1
omarferndz <omarferndz@gmail.com>
2019-05-08 11:38:13 -0400

    manejo de cadenas 2

---
+++ manejo_cadenas2.php
@@ -0,0 +1,16 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+sprintf(cadena de formato, var1, var2...). Formatea una cadena de texto al igual que printf pero el resultado es devuelto como una cadena.
+*/
+$resultado=sprintf("8x5 = %d <br>",8*5);
+echo $resultado,"<br>";
+
+?>
+</body>
+</html>
