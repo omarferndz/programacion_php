cb7b0963518376ce3e3e47a52894d5e41f800018
omarferndz <omarferndz@gmail.com>
2019-05-08 11:39:15 -0400

    manejo de cadenas 6

---
+++ manejo_cadenas6.php
@@ -0,0 +1,16 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+str_replace(cadena1, cadena2, texto). Reemplaza la cadena1 por la cadena2 en el texto.
+*/
+echo str_replace("verde","rojo","Un pez de color verde, como verde es la
+hierba."),"<br>";
+
+?>
+</body>
+</html>
