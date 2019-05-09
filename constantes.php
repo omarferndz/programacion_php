0f671e0cefa3d130bf9dd6d481d90aec90d67337
omarferndz <omarferndz@gmail.com>
2019-05-09 10:22:15 -0400

    constantes

---
+++ constantes.php
@@ -0,0 +1,17 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+las constantes se definen con la funcion define():
+*/
+define("SALUDO", "Hola, mundo!");
+
+echo "La constante SALUDO vale " . SALUDO;
+
+?>
+</body>
+</html>
