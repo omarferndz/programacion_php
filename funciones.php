5289e08c4822ca8409f66a48e3264f02a12daa60
omarferndz <omarferndz@gmail.com>
2019-05-09 12:44:53 -0400

    funciones

---
+++ funciones.php
@@ -0,0 +1,23 @@
+<!-- Manual de PHP -->
+<html>
+<head>
+	<title> Ejemplo de PHP</title>
+</head>
+<body>
+<?php
+/*
+
+*/
+
+	function media_aritmetica($a, $b)
+	{
+		$media=($a+$b) /2;
+		return $media;
+	}
+
+	echo media_aritmetica(4,6),"<br>";
+	echo media_aritmetica(3242,524543),"<br>";
+
+?>
+</body>
+</html>
